25c4f1c467c57a0b462482a57380509ee21f69b3
Graeme Corrigan <corrigangk@gmail.com>
2023-10-14 15:38:30 -0400

    Completed exercise cleaned up and autoformatted

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   const [pokemonData, setPokemonData] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [typeFilter, setTypeFilter] = useState('');
-  const [weaknessFilter, setWeaknessFilter] = useState('');
-  const [backgroundColor, setBackgroundColor] = useState('gray');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [typeFilter, setTypeFilter] = useState("");
+  const [weaknessFilter, setWeaknessFilter] = useState("");
+  const [backgroundColor, setBackgroundColor] = useState("gray");
 
   useEffect(() => {
-    // Fetch data from the API and update pokemonData state
-    // Use the provided API endpoint
-    fetch('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json')
+    fetch(
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
+    )
       .then((response) => response.json())
       .then((data) => setPokemonData(data.pokemon));
   }, []);
 
   const filteredPokemon = pokemonData.filter((pokemon) => {
-    const nameMatch = pokemon.name.toLowerCase().includes(searchTerm.toLowerCase());
+    const nameMatch = pokemon.name
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
     const typeMatch = !typeFilter || pokemon.type.includes(typeFilter);
-    const weaknessMatch = !weaknessFilter || pokemon.weaknesses.includes(weaknessFilter);
+    const weaknessMatch =
+      !weaknessFilter || pokemon.weaknesses.includes(weaknessFilter);
 
     return nameMatch && typeMatch && weaknessMatch;
   });
 
   const typeColorMatch = {
-    fire: 'red',
-    water: 'blue',
-    grass: 'green',
-    ground: 'brown',
-    poison: 'purple',
-    ice: 'lightblue',
-    normal: 'beige',
-    electric: 'yellow',
-    bug: 'orange',
-    
+    fire: "red",
+    water: "blue",
+    grass: "green",
+    ground: "brown",
+    poison: "purple",
+    ice: "lightblue",
+    normal: "beige",
+    electric: "yellow",
+    bug: "orange",
   };
 
   useEffect(() => {
+    try {
+      if (filteredPokemon.length > 0) {
+        const firstPokeType = filteredPokemon[0].type[0].toLowerCase();
+        const backGColor = typeColorMatch[firstPokeType] || "gray";
 
-    try{
-    if (filteredPokemon.length > 0){
-      const firstPokeType = filteredPokemon[0].type[0].toLowerCase();
-      const backGColor = typeColorMatch[firstPokeType] || 'gray';
-
-      
-
-      setBackgroundColor(backGColor)
-    }} catch (error){
-      console.error('Error setting background color:', error);
-      setBackgroundColor('gray');
+        setBackgroundColor(backGColor);
+      }
+    } catch (error) {
+      console.error("Error setting background color:", error);
+      setBackgroundColor("gray");
     }
   }, [filteredPokemon]);
   return (
-    <div className="App" style={{backgroundColor}}>
+    <div className="App" style={{ backgroundColor }}>
       <div>
         <label>
           Search by Name:
@@ -87,11 +87,14 @@ function App() {
       {/* Render the filteredPokemon list */}
       {filteredPokemon.map((pokemon) => (
         <div key={pokemon.id}>
-          <img src={`http://www.serebii.net/pokemongo/pokemon/${pokemon.num}.png`} alt="{pokemon.name}" />
+          <img
+            src={`http://www.serebii.net/pokemongo/pokemon/${pokemon.num}.png`}
+            alt="{pokemon.name}"
+          />
           <p>Name: {pokemon.name}</p>
           <p>Number: {pokemon.num}</p>
-          <p>Type: {pokemon.type.join(', ')}</p>
-          <p>Weaknesses: {pokemon.weaknesses.join(', ')}</p>
+          <p>Type: {pokemon.type.join(", ")}</p>
+          <p>Weaknesses: {pokemon.weaknesses.join(", ")}</p>
         </div>
       ))}
     </div>
